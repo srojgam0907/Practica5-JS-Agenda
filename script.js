9515588fd175c0d73e37e2e99378c9eb38aa6526
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-17 13:49:19 +0100

    constantes y primeras funciones

---
+++ script.js
@@ -0,0 +1,46 @@
+//CONSTANTES GLOBALES
+const form= document.querySelector("#formContacto");
+const btnAddTel= document.querySelector("#btnAddTel");
+const mensaje= document.querySelector("#msg");
+const btnBorrarTodo= document.querySelector("#btnBorrarTodo");
+
+const nombre= document.querySelector("#inpNombre");
+const apellidos= document.querySelector("#inpApellidos");
+const telefono= document.querySelector("#inpTelNumero");
+
+let agenda= [];
+
+//EVENTOS
+form.addEventListener("submit", () => {
+
+});
+
+
+
+//FUNCIONES
+function validate() {
+    if (!nombre.checkValidity()) {
+        nombre.reportValidity();
+        return;
+    }
+
+    if (!apellidos.checkValidity()) {
+        apellidos.reportValidity();
+        return;
+    }
+
+}
+
+function cargarContactos() {
+  const raw = localStorage.getItem("agenda");
+
+  if (!raw) return [];
+
+  try {
+    const data = JSON.parse(raw);
+    return Array.isArray(data) ? data : [];
+  } catch (error) {
+    console.error("JSON inválido:", error);
+    return [];
+  }
+}
